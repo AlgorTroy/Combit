21ec15a6fa89f62f890f844f927b692ad2ffcd66
AlgorXT <algortroy@gmail.com>
2017-02-18 16:27:41 +0530

    integrating login

--- MainApp/static/materialize/js/init.js
+++ MainApp/static/materialize/js/init.js
@@ -3,9 +3,11 @@ $(document).ready(function() {
 
     $(window).scroll(function() { // check if scroll event happened
         if ($(document).scrollTop() > 10) { // check if user scrolled more than 50 from top of the browser window
-          $(".nav-wrapper").css("background-color", "rgba(36,49,60,0.7)"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+            $(".nav-wrapper").css("background-color", "rgba(36,49,60,0.7)"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
         } else {
-          $(".nav-wrapper").css("background-color", "transparent"); // if not, change it back to transparent
+            $(".nav-wrapper").css("background-color", "transparent"); // if not, change it back to transparent
         }
-      });
+    });
+    $('ul.tabs').tabs();
+    $('.modal').modal();
 });

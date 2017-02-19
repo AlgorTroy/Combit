08e96e0efe2519164e528a8e089be6e7c160e37e
AlgorXT <algortroy@gmail.com>
2017-02-19 20:02:49 +0530

    New dropdown for login-sign up cycle

--- MainApp/static/materialize/js/init.js
+++ MainApp/static/materialize/js/init.js
@@ -49,4 +49,17 @@ $(document).ready(function() {
       $(this).removeClass(ctrg_exp_icon_anim);
     });
 
+    $('.dropdown-button').dropdown({
+      inDuration: 100,
+      outDuration: 300,
+      constrainWidth: false, // Does not change width of dropdown to that of the activator
+      hover: false, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+      stopPropagation: false // Stops event propagation
+    }
+  );
+
+
 });
